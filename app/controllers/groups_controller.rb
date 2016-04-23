5195608bfb1f3d2ab9eca084976c22b28a455d1f
AhmedGouda77 <ahmed99kamal@gmail.com>
2016-04-23 19:10:46 +0200

    authentication for user

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,4 +1,5 @@
 class GroupsController < ApplicationController
+	  before_action :authenticate_user!, only: [:index, :new, :show, :edit, :update, :destroy]
   before_action :set_group, only: [:show, :edit, :update, :destroy]
 
   # GET /groups
