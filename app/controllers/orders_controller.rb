df26a7196ef10abdc244c9a799768d2c58ceff19
Eng Omar Mohamed <o.mohamed10@gmail.com>
2016-04-26 23:39:56 +0200

    modify in add order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -34,9 +34,25 @@ class OrdersController < ApplicationController
   # POST /orders.json
   def create
    # @order = Order.new(order_params)
-  @order = current_user.orders.build(order_params)
+  # @order = current_user.orders.build(order_params)
+  #   respond_to do |format|
+  #     if @order.save
+  @order = Order.new(order_params)
+    @order.user_id = current_user.id
+    @order.status = 0
+    users_arr = "[" + params[:users] + "]"
+    users_ids = eval(users_arr)
+
+    group_users_arr = "[" + params[:group_users] + "]"
+    group_users_ids = eval(group_users_arr)
+
+    users_ids.push(*group_users_ids)
+    # abort()
     respond_to do |format|
       if @order.save
+        users_ids.uniq.each do |id|
+          orders_user = OrdersUser.new( :order_id => @order.id , :user_id => id , :is_joined => false ).save
+        end
         format.html { redirect_to @order, notice: 'Order was successfully created.' }
         format.json { render :show, status: :created, location: @order }
       else
