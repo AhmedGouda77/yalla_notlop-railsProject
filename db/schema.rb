5f3ab8ece6a20831de56af480c01f9d3b7276875
Eng Omar Mohamed <o.mohamed10@gmail.com>
2016-04-28 17:25:10 +0200

    modification

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,24 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160422142710) do
+ActiveRecord::Schema.define(version: 20160427211329) do
+
+  create_table "activities", force: :cascade do |t|
+    t.integer  "trackable_id",   limit: 4
+    t.string   "trackable_type", limit: 255
+    t.integer  "owner_id",       limit: 4
+    t.string   "owner_type",     limit: 255
+    t.string   "key",            limit: 255
+    t.text     "parameters",     limit: 65535
+    t.integer  "recipient_id",   limit: 4
+    t.string   "recipient_type", limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "activities", ["owner_id", "owner_type"], name: "index_activities_on_owner_id_and_owner_type", using: :btree
+  add_index "activities", ["recipient_id", "recipient_type"], name: "index_activities_on_recipient_id_and_recipient_type", using: :btree
+  add_index "activities", ["trackable_id", "trackable_type"], name: "index_activities_on_trackable_id_and_trackable_type", using: :btree
 
   create_table "follows", force: :cascade do |t|
     t.integer  "followable_id",   limit: 4,                   null: false
@@ -35,16 +52,18 @@ ActiveRecord::Schema.define(version: 20160422142710) do
 
   add_index "groups", ["user_id"], name: "index_groups_on_user_id", using: :btree
 
-  create_table "notification", force: :cascade do |t|
-    t.integer  "user_id",      limit: 4
-    t.string   "notification", limit: 255
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
+  create_table "notifications", force: :cascade do |t|
+    t.integer  "recipient_id",    limit: 4
+    t.integer  "actor_id",        limit: 4
+    t.datetime "read_at"
+    t.string   "action",          limit: 255
+    t.integer  "notifiable_id",   limit: 4
+    t.string   "notifiable_type", limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
-  add_index "notification", ["user_id"], name: "index_notification_on_user_id", using: :btree
-
-  create_table "order_details", id: false, force: :cascade do |t|
+  create_table "order_details", force: :cascade do |t|
     t.string   "item",       limit: 255
     t.integer  "price",      limit: 4
     t.integer  "amount",     limit: 4
@@ -60,12 +79,17 @@ ActiveRecord::Schema.define(version: 20160422142710) do
   add_index "order_details", ["user_id"], name: "index_order_details_on_user_id", using: :btree
 
   create_table "orders", force: :cascade do |t|
-    t.string   "image",      limit: 255
-    t.integer  "for",        limit: 4
-    t.string   "from",       limit: 255
-    t.integer  "user_id",    limit: 4
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "image",               limit: 255
+    t.integer  "for",                 limit: 4
+    t.string   "from",                limit: 255
+    t.integer  "status",              limit: 4
+    t.integer  "user_id",             limit: 4
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
   end
 
   add_index "orders", ["user_id"], name: "index_orders_on_user_id", using: :btree
@@ -103,13 +127,14 @@ ActiveRecord::Schema.define(version: 20160422142710) do
     t.string   "avatar_content_type",    limit: 255
     t.integer  "avatar_file_size",       limit: 4
     t.datetime "avatar_updated_at"
+    t.string   "provider",               limit: 255
+    t.string   "uid",                    limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "groups", "users"
-  add_foreign_key "notification", "users"
   add_foreign_key "order_details", "orders"
   add_foreign_key "order_details", "users"
   add_foreign_key "orders", "users"
